33190f57b3c8098f357dc3d775296c90c02ca6f7
sajedeh1993 <sa.mohamadzadeh1993@gmail.com>
2018-12-26 17:53:06 +0330

    addapter layout completed

--- app/src/main/java/com/sajedeh/app/pet/PetAdapter.java
+++ app/src/main/java/com/sajedeh/app/pet/PetAdapter.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PetAdapter extends CursorAdapter {
     public PetAdapter(Context context, Cursor c) {
@@ -15,12 +17,34 @@ public class PetAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
 
-
-        return LayoutInflater.from(context).inflate(R.layout.pet_item,viewGroup, false);
+        return LayoutInflater.from(context).inflate(R.layout.pet_item, viewGroup, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        String name = cursor.getString(cursor.getColumnIndex(PetContract.PetEntity.COLUMN_NAME));
+        int age = cursor.getInt(cursor.getColumnIndex(PetContract.PetEntity.COLUMN_AGE));
+        int pic = cursor.getInt(cursor.getColumnIndex(PetContract.PetEntity.COLUMN_PIC));
+
+        TextView nameTextView = view.findViewById(R.id.name);
+        nameTextView.setText(name);
+
+        TextView ageTextView = view.findViewById(R.id.age);
+        nameTextView.setText(Integer.toString(age));
+
+        ImageView picImageView = view.findViewById(R.id.picture);
+
+        switch (pic) {
+            case PetContract.PetEntity.PIC_CAT:
+                picImageView.setImageResource(R.drawable.ic_cat);
+                break;
+            case PetContract.PetEntity.PIC_DOG:
+                picImageView.setImageResource(R.drawable.ic_dog);
+                break;
+            case PetContract.PetEntity.PIC_MOUSE:
+                picImageView.setImageResource(R.drawable.ic_mouse);
+                break;
+        }
 
     }
 }
