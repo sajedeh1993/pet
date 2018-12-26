b4ffbfb0d6cf0aa9bbab8d6a30fbed2ff64495df
sajedeh1993 <sa.mohamadzadeh1993@gmail.com>
2018-12-26 17:36:14 +0330

    new view completed

--- app/src/main/java/com/sajedeh/app/pet/PetAdapter.java
+++ app/src/main/java/com/sajedeh/app/pet/PetAdapter.java
@@ -2,6 +2,7 @@ package com.sajedeh.app.pet;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
@@ -15,7 +16,7 @@ public class PetAdapter extends CursorAdapter {
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
 
 
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.pet_item,viewGroup, false);
     }
 
     @Override
